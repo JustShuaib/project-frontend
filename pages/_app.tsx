e37af10d5e187b57259ddec3f30396ca9cd4b0b7
Adeoti Shuaib <Adeotishuaib2001@gmail.com>
2022-11-17 20:43:11 +0100

    Implemented sign up, changed font

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,22 @@
-import { ChakraProvider } from "@chakra-ui/react";
 import type { AppProps } from "next/app";
-
+import { Open_Sans } from "@next/font/google";
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
+const openSans = Open_Sans();
+const myTheme = extendTheme({
+  styles: {
+    global: {
+      body: {
+        fontFamily: openSans.style.fontFamily,
+      },
+      "input::-ms-reveal, input::-ms-clear": {
+        display: "none",
+      },
+    },
+  },
+});
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider>
+    <ChakraProvider theme={myTheme}>
       <Component {...pageProps} />
     </ChakraProvider>
   );
--- pages/signup.tsx
+++ pages/signup.tsx
@@ -1,47 +1,171 @@
+import { useState } from "react";
 import {
   Box,
   Button,
   Container,
   Flex,
+  FormControl,
+  FormErrorMessage,
+  FormLabel,
   Heading,
+  IconButton,
   Input,
-  Stack,
+  InputGroup,
+  InputRightElement,
   Text,
 } from "@chakra-ui/react";
+import { useForm, SubmitHandler } from "react-hook-form";
+import { Eye, EyeClose } from "../icons";
 import { Link, Page } from "../components";
-
+interface FormData {
+  username: string;
+  email: string;
+  password: string;
+  confirm_password: string;
+}
 const SignUp = () => {
+  const [show, setShow] = useState({ pWord: false, cpWord: false });
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    reset,
+    getValues,
+  } = useForm<FormData>();
+  const password = getValues("password");
+
+  const onSubmit: SubmitHandler<FormData> = async (data) => {
+    const res = await fetch("https://e-commerce-66n3.onrender.com/users", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(data),
+    });
+    const json = await res.json();
+    console.log(json);
+    reset();
+    setShow({ pWord: false, cpWord: false });
+  };
   return (
     <Page title="Sign up">
-      <Box as="main">
+      <Box as="main" mt={6}>
         <Heading textAlign="center">Sign Up</Heading>
-        <Text>Sign up on the platform</Text>
-        <Container as="form">
-          <Flex gap={3}>
-            <Stack flexBasis="100%" spacing={1}>
-              <label htmlFor="name">Name</label>
-              <Input placeholder="name" />
-            </Stack>
-            <Stack flexBasis="100%" spacing={1}>
-              <label htmlFor="email">Email</label>
-              <Input type="email" placeholder="email" />
-            </Stack>
+        <Text textAlign="center">Sign up on the platform</Text>
+
+        <Container
+          as="form"
+          maxW="container.sm"
+          onSubmit={handleSubmit(onSubmit)}
+        >
+          <Flex direction={["column", "row"]} gap={[2, 3]} mb={[2]}>
+            <FormControl flexBasis="100%" isInvalid={Boolean(errors.username)}>
+              <FormLabel htmlFor="username">Username</FormLabel>
+              <Input
+                id="username"
+                {...register("username", {
+                  required: "Username is required",
+                  minLength: {
+                    value: 3,
+                    message: "Username must be at least 3 characters",
+                  },
+                })}
+                placeholder="Choose a username"
+              />
+              <FormErrorMessage>
+                {errors.username && errors.username.message}
+              </FormErrorMessage>
+            </FormControl>
+
+            <FormControl isInvalid={Boolean(errors.email)}>
+              <FormLabel htmlFor="email">Email</FormLabel>
+              <Input
+                id="email"
+                {...register("email", {
+                  required: "Email is required",
+                  pattern: {
+                    value: /^\S+@\S+$/i,
+                    message: "Invalid email address",
+                  },
+                })}
+                placeholder="Email Address"
+              />
+              <FormErrorMessage>
+                {errors.email && errors.email.message}
+              </FormErrorMessage>
+            </FormControl>
           </Flex>
-          <Flex gap={3}>
-            <Stack flexBasis="100%" spacing={1}>
-              <label htmlFor="password">Password</label>
-              <Input type="password" placeholder="password" />
-            </Stack>
-            <Stack flexBasis="100%" spacing={1}>
-              <label htmlFor="email">Confirm Password</label>
-              <Input type="password" placeholder="confirm password" />
-            </Stack>
+
+          <Flex direction={["column", "row"]} gap={[2, 3]}>
+            <FormControl isInvalid={Boolean(errors.password)}>
+              <FormLabel htmlFor="password">Password</FormLabel>
+              <InputGroup>
+                <Input
+                  id="password"
+                  {...register("password", {
+                    required: "Password is required",
+                    pattern: {
+                      value: /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{6,}$/,
+                      message:
+                        "Password must be at least 6 characters, contain at least one uppercase letter, one lowercase letter, and one number",
+                    },
+                    maxLength: {
+                      value: 20,
+                      message: "Password must be at most 20 characters",
+                    },
+                  })}
+                  type={show.pWord ? "text" : "password"}
+                  placeholder="Enter password"
+                />
+                <InputRightElement>
+                  <IconButton
+                    aria-label="toggle password visibility"
+                    icon={show.pWord ? <EyeClose /> : <Eye />}
+                    onClick={() =>
+                      setShow((show) => ({
+                        pWord: !show.pWord,
+                        cpWord: show.cpWord,
+                      }))
+                    }
+                  ></IconButton>
+                </InputRightElement>
+              </InputGroup>
+              <FormErrorMessage>
+                {errors.password && errors.password.message}
+              </FormErrorMessage>
+            </FormControl>
+
+            <FormControl isInvalid={Boolean(errors.confirm_password)}>
+              <FormLabel htmlFor="confirmPassword">Password</FormLabel>
+              <InputGroup>
+                <Input
+                  id="confirmPassword"
+                  {...register("confirm_password", {
+                    validate: (value) =>
+                      value === password || "The passwords do not match",
+                    required: "Confirm Password",
+                  })}
+                  type={show.cpWord ? "text" : "password"}
+                  placeholder="Confirm Password"
+                />
+                <InputRightElement>
+                  <IconButton
+                    aria-label="toggle password visibility"
+                    icon={show.cpWord ? <EyeClose /> : <Eye />}
+                    onClick={() =>
+                      setShow((show) => ({
+                        cpWord: !show.cpWord,
+                        pWord: show.pWord,
+                      }))
+                    }
+                  ></IconButton>
+                </InputRightElement>
+              </InputGroup>
+              <FormErrorMessage>
+                {errors.confirm_password && errors.confirm_password.message}
+              </FormErrorMessage>
+            </FormControl>
           </Flex>
-          <Stack spacing={1} mt={2}>
-            <label htmlFor="password">Password</label>
-            <Input type="email" placeholder="password" />
-          </Stack>
-          <Button type="submit" w="full" mt={4} py={5}>
+          <Button type="submit" w="full" mt={4}>
             Sign Up
           </Button>
           <Text>
