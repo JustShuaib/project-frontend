eac29f75b9ce85bcc97d716eef11a986b264c4e1
Adeoti Shuaib <Adeotishuaib2001@gmail.com>
2022-11-23 22:21:48 +0100

    Added animation to heading elements

---
+++ components/heading.tsx
@@ -0,0 +1,39 @@
+import { ReactNode } from "react";
+import { Heading as ChakraHeading } from "@chakra-ui/react";
+import type { As } from "@chakra-ui/react";
+import { motion } from "framer-motion";
+const Heading = ({
+  as,
+  size,
+  children,
+}: {
+  as: As;
+  size: string;
+  children: ReactNode;
+}) => {
+  return (
+    <motion.div
+      initial="hidden"
+      animate="visible"
+      variants={{
+        hidden: {
+          scale: 0.6,
+          opacity: 0,
+        },
+        visible: {
+          scale: 1,
+          opacity: 1,
+          transition: {
+            delay: 0.4,
+          },
+        },
+      }}
+    >
+      <ChakraHeading as={as} mb={2} size={size} textAlign="center">
+        {children}
+      </ChakraHeading>
+    </motion.div>
+  );
+};
+
+export default Heading;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,28 @@
 import type { AppProps } from "next/app";
 import { ChakraProvider } from "@chakra-ui/react";
+import { motion } from "framer-motion";
 import { theme } from "../utils/theme";
-export default function App({ Component, pageProps }: AppProps) {
+export default function App({ Component, pageProps, router }: AppProps) {
   return (
-    <ChakraProvider theme={theme}>
-      <Component {...pageProps} />
-    </ChakraProvider>
+    <motion.div
+      key={router.route}
+      initial="initial"
+      animate="animate"
+      variants={{
+        initial: {
+          opacity: 0,
+        },
+        animate: {
+          opacity: 1,
+          transition: {
+            delay: 0.4,
+          },
+        },
+      }}
+    >
+      <ChakraProvider theme={theme}>
+        <Component {...pageProps} />
+      </ChakraProvider>
+    </motion.div>
   );
 }
