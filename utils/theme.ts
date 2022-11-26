a242b9e9033170a562e1502be5c79b65c66660ac
Adeoti Shuaib <Adeotishuaib2001@gmail.com>
2022-11-26 13:12:02 +0100

    Styled scroll bar

--- utils/theme.ts
+++ utils/theme.ts
@@ -1,14 +1,44 @@
 import { extendTheme, defineStyleConfig } from "@chakra-ui/react";
-import { Open_Sans, Roboto } from "@next/font/google";
+import {
+  Open_Sans,
+  Roboto,
+  Ubuntu_Mono,
+  Cedarville_Cursive,
+} from "@next/font/google";
 const openSans = Open_Sans();
 const roboto = Roboto({ weight: "700" });
+const ubuntuMono = Ubuntu_Mono({ weight: "400" });
+const cedarvilleCursive = Cedarville_Cursive({ weight: "400" });
 const Heading = defineStyleConfig({
   baseStyle: {
     fontFamily: roboto.style.fontFamily,
     textAlign: "center",
   },
+  variants: {
+    h2: {
+      fontSize: "lg",
+      fontFamily: cedarvilleCursive.style.fontFamily,
+      color: "red.700",
+      textAlign: "left",
+    },
+  },
+});
+const Button = defineStyleConfig({
+  baseStyle: {
+    // color: "white",
+    // bg: "#0099ff",
+    fontFamily: ubuntuMono.style.fontFamily,
+    backgroundColor: "blue.500",
+    _hover: {
+      bg: "#0066cc",
+    },
+  },
 });
 export const theme = extendTheme({
+  // colors: {
+  //   // primary: "#0099ff",
+  //   // :"#0099ff"
+  // },
   components: {
     toast: {
       baseStyle: {
@@ -20,11 +50,25 @@ export const theme = extendTheme({
       },
     },
     Heading: Heading,
+    Button: Button,
   },
   styles: {
     global: {
       body: {
         fontFamily: openSans.style.fontFamily,
+      },
+      "::-webkit-scrollbar": {
+        width: "0.6em",
+      },
+      "::-webkit-scrollbar-track": {
+        borderRadius: "0.5rem",
+        backgroundColor: "gray.100",
+        border: "1px solid gray.300",
+        boxShadow: "inset 0 0 6px rgba(0, 0, 0, .3)",
+      },
+      "::-webkit-scrollbar-thumb": {
+        borderRadius: "0.6rem",
+        backgroundColor: "#0099ff",
       },
       "input::-ms-reveal, input::-ms-clear": {
         display: "none",
