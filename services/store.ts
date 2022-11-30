b5a8afc237c589577d87972f1e94bae0657ac506
Adeoti Shuaib <Adeotishuaib2001@gmail.com>
2022-11-30 07:11:00 +0100

    integrated trk query with the store

--- services/store.ts
+++ services/store.ts
@@ -1,10 +1,15 @@
 import { configureStore } from "@reduxjs/toolkit";
 import loginReducer from "./slices/loginSlice";
+import { api } from "./api";
 
 export const store = configureStore({
   reducer: {
+    [api.reducerPath]: api.reducer,
     login: loginReducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(api.middleware),
+  devTools: process.env.NODE_ENV !== "production",
 });
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
